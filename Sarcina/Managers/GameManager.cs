486d7286aef1e802584681a6f6b5f1927bf6e28d
Pawo1 <pawo70@wp.pl>
2022-02-13 16:15:42 +0100

    Keyboard handling

--- Sarcina/Managers/GameManager.cs
+++ Sarcina/Managers/GameManager.cs
@@ -10,6 +10,11 @@ using Sarcina.CustomSerializators;
 using Sarcina.Maps;
 using Sarcina.Objects;
 
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
 namespace Sarcina.Managers
 {
     public class GameManager
@@ -19,9 +24,65 @@ namespace Sarcina.Managers
         private Dictionary<string, GameObjectProps> dictBuffer;
         private GraphicManager graphicManager;
 
+        private List<Keyboard.Key> secretCodes;
+        private Clock secretClock;
+        private Clock keyClock;
+
+
         public GameManager()
         {
+            keyClock = new Clock();
+            secretClock = new Clock();
+            secretCodes = new List<Keyboard.Key>();
+        }
 
+
+        public void OnKeyPressed(object sender, KeyEventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+
+            if (keyClock.ElapsedTime >= Time.FromMilliseconds(100))
+            {
+                Console.WriteLine(e.Code);
+                switch (e.Code)
+                {
+                    case Keyboard.Key.Escape:
+                        window.Close();
+                        break;
+
+                    case Keyboard.Key.Up:
+                        if(secretClock.ElapsedTime > Time.FromSeconds(3))
+                        {
+                            secretClock.Restart();
+                            secretCodes.Clear();
+                        }
+                        secretCodes.Add(Keyboard.Key.Up);
+                        break;
+                    case Keyboard.Key.Down:
+                        secretCodes.Add(Keyboard.Key.Down);
+                        break;
+                    case Keyboard.Key.Left:
+                        secretCodes.Add(Keyboard.Key.Left);
+                        break;
+                    case Keyboard.Key.Right:
+                        secretCodes.Add(Keyboard.Key.Right);
+                        break;
+                    case Keyboard.Key.B:
+                        secretCodes.Add(Keyboard.Key.B);
+                        break;
+                    case Keyboard.Key.A:
+                        secretCodes.Add(Keyboard.Key.A);
+                        if(secretClock.ElapsedTime <= Time.FromSeconds(6))
+                        {
+                            CheckKonami(new List<Keyboard.Key>(secretCodes.TakeLast(10)));
+                        }
+                        break;
+                }
+
+                keyClock.Restart();
+            }
+
+               
         }
 
         public void LoadMap(string path)
@@ -54,6 +115,19 @@ namespace Sarcina.Managers
         {
             map = CopyMap(mapRestorationBuffer);
             GameObject.UpdateDictionary(CopyDict(dictBuffer));
+        }
+
+
+        private void CheckKonami(List<Keyboard.Key> secretCodes)
+        {
+            if(secretCodes[0] == Keyboard.Key.Up && secretCodes[1] == Keyboard.Key.Up &&
+                secretCodes[2] == Keyboard.Key.Down && secretCodes[3] == Keyboard.Key.Down &&
+                secretCodes[4] == Keyboard.Key.Left && secretCodes[5] == Keyboard.Key.Right &&
+                secretCodes[6] == Keyboard.Key.Left && secretCodes[7] == Keyboard.Key.Right &&
+                secretCodes[8] == Keyboard.Key.B && secretCodes[9] == Keyboard.Key.A)
+            {
+                Console.WriteLine("You activated my trap CARD!");
+            }
         }
 
         private  Map CopyMap(Map original)
--- Sarcina/Program.cs
+++ Sarcina/Program.cs
@@ -3,13 +3,31 @@ using Sarcina.Maps;
 using Sarcina.Objects;
 using System.Diagnostics;
 
+using Sarcina.Managers;
+
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
 namespace Sarcina
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameManager gameManager = new GameManager();
+            GraphicManager graphicManager = new GraphicManager();
+
+            RenderWindow window = new RenderWindow(new VideoMode(720, 480, 64), "Sarcina The Game", Styles.Default , new ContextSettings(24, 8, 16));
+            window.SetFramerateLimit(60);
+            window.KeyPressed += new EventHandler<KeyEventArgs>(gameManager.OnKeyPressed); // register key handler 
+            graphicManager.AttachWindow(window);
+
+            graphicManager.Demo();
+
         }
+
+
+
     }
 }
